fb6c9993b28741e3561d5f46ca768a0435cbdd95
desata <desata@gmail.com>
2022-03-15 15:29:07 +0200

    rev

--- ObjectsAndClasses/06.StoreBoxes/Program.cs
+++ ObjectsAndClasses/06.StoreBoxes/Program.cs
@@ -1,36 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.StoreBoxes
 {
     public class Item
     {
         //Define a class Item, which contains these properties: Name and Price.
-
         public string ItemName { get; set; }
 
         public double ItemPrice { get; set; }
-
-        
+                
     }
     public class Box 
     {
+
         //Define a class Box, which contains these properties: Serial Number, Item, Item Quantity, and Price for a Box.
+        public string BoxSerialNumber { get; set; }
 
-        public int BoxSerialNumber { get; set; }
-
-        public string BoxItem { get; set; }
+        public Item BoxItem { get; set; }
 
         public int BoxItemQty { get; set; }
 
         public double BoxPrice { get; set; }
     }   
 
-
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Until you receive "end", you will be receiving data in the following format: "{Serial Number} {Item Name} {Item Quantity} {itemPrice}"
+            //The Price of one box has to be calculated: itemQuantity * itemPrice.
+            //Print all the boxes, ordered descending by price for a box, in the following format: 
+            //{ boxSerialNumber}
+            //-- { boxItemName} – ${ boxItemPrice}: { boxItemQuantity}
+            //-- ${ boxPrice}
+            //The price should be formatted to the 2nd digit after the decimal separator.
+
+            List<Box> storeBoxes = new List<Box>();
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] arguments = command.Split();
+ 
+                int itemQty = Convert.ToInt32(arguments[2]);
+                double itemPrice = Convert.ToDouble(arguments[3]);
+                double boxPrice = itemQty * itemPrice;
+
+                Box boxes = new Box();
+                Item items = new Item();
+
+                items.ItemName = arguments[1];
+                items.ItemPrice = itemPrice;
+                boxes.BoxItem = items;
+                boxes.BoxSerialNumber = arguments[0]; 
+                boxes.BoxItemQty = itemQty;
+                boxes.BoxPrice = boxPrice;
+               
+                storeBoxes.Add(boxes);
+
+                command = Console.ReadLine();
+            }
+
+            foreach (Box stbox in storeBoxes.OrderByDescending(x => x.BoxPrice))
+            {
+                Console.WriteLine($"{stbox.BoxSerialNumber}\n-- {stbox.BoxItem.ItemName} - ${stbox.BoxItem.ItemPrice:F2}: {stbox.BoxItemQty}\n-- ${stbox.BoxPrice:F2}");
+            }
 
         }
+
     }
+
 }
---
+++ ObjectsAndClasses/VehicleCatalogue/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VehicleCatalogue
+{
+    class Car
+    {
+        //Brand, Model, and Horse Power
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+    }
+    class Truck
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Weight { get; set; }
+    }
+    class Catalog
+    {
+        public List<Car> Cars { get; set; }
+        public List<Truck> Trucks { get; set; }
+        public Catalog()
+        {
+            Cars = new List<Car>();
+
+            Trucks = new List<Truck>();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("/");
+            Catalog catalog = new Catalog();
+
+            while (input[0] != "end")
+            {
+
+                Truck trucks = new Truck();
+
+                if (input[0] == "Car")
+                {
+                    Car cars = new Car();
+                    cars.Brand = input[1];
+                    cars.Model = input[2];
+                    cars.HorsePower = int.Parse(input[3]);
+                    catalog.Cars.Add(cars);
+
+                }
+                else if (input[0] == "Truck")
+                {
+                    trucks.Brand = input[1];
+                    trucks.Model = input[2];
+                    trucks.Weight = int.Parse(input[3]);
+                    catalog.Trucks.Add(trucks);
+
+                }
+                input = Console.ReadLine().Split("/");
+
+            }
+
+            if (catalog.Cars.Count > 0)
+            {
+                Console.WriteLine("Cars:");
+                foreach (var car in catalog.Cars.OrderBy(car => car.Brand))
+                {
+                    Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+                }
+            }
+            if (catalog.Trucks.Count > 0)
+            {
+                Console.WriteLine("Trucks:");
+                foreach (var truck in catalog.Trucks.OrderBy(truck => truck.Brand))
+                {
+                    Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+                }
+            }
+
+        }
+    }
+}
